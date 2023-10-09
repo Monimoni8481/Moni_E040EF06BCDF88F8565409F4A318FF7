a8f355311e797c35a0de939be30fda61d5b79c4a
Monimoni8481 <144675096+Monimoni8481@users.noreply.github.com>
2023-10-08 22:41:29 -0700

    Add files via upload

---
+++ main (5).py
@@ -0,0 +1,19 @@
+class player:
+
+  def play(self):
+     print("The player is playing cricket.")
+
+class Batsman:
+
+   def play(self):
+      print("The bowler is bowling.")
+
+class Bowler(player):
+
+   def player(self):
+     print("The Bowler is bowling.")
+
+batsman=Batsman()
+bowler= Bowler()
+batsman. play()
+bowler. play()
